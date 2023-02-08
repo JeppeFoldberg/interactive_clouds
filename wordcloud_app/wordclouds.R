c007ee011d9b88ec29dbc103e877e32e7e0acd85
Jeppe Foldberg <jeppe_05@msn.com>
2023-02-08 10:10:46 +0100

    Started implementing reactive wordcloud; missing proper wordcount function

--- wordcloud_app/wordclouds.R
+++ wordcloud_app/wordclouds.R
@@ -1,4 +1,11 @@
-library(shiny)
+pacman::p_load(
+  shiny,
+  wordcloud2,
+  tm,
+  memoise
+)
+# library(shiny)
+# library(wordcloud2)
 
 # Makes sure that we can upload larger files - here 30 mb! 
 options(shiny.maxRequestSize = 30*1024^2)
@@ -9,14 +16,14 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
     # Input: Select a file ----
-    fileInput("file1", "Choose sav file",
+    fileInput("file", "Choose sav file",
               multiple = F,
               accept = c(".sav")),
     
     # Horizontal line ----
     tags$hr(),
     
-    textInput("textcolumn", "Navn på tekst-kolonne"),
+    textInput("textcolumn", "Navn paa tekst-kolonne"),
     
     sliderInput("freq",
                 "Minimum frekvens:",
@@ -27,12 +34,52 @@ ui <- fluidPage(
     ),
 
    
-    mainPanel("Wordcloud")
+    mainPanel(plotOutput("plot"))
   )
 )
 
 server <- function(input, output) {
+  terms <- reactive({
+    # Change when the "update" button is pressed...
+    input$update
+    # ...but not for anything else
+    isolate({
+      withProgress({
+        setProgress(message = "Processing corpus...")
+        file <- input$file
+        getTermMatrix(file[input$textcolumn])
+      })
+    })
+  })
   
+  # Make the wordcloud drawing predictable during a session
+  wordcloud_rep <- repeatable(wordcloud2)
+  
+  output$plot <- renderPlot({
+    v <- terms()
+    wordcloud_rep(names(v), v, scale=c(4,0.5),
+                  min.freq = input$freq, max.words=input$max,
+                  colors=brewer.pal(8, "Dark2"))
+  })
 }
+
+# Using "memoise" to automatically cache the results
+getTermMatrix <- memoise(function(text) {
+  
+  myCorpus = Corpus(VectorSource(text))
+  myCorpus = tm_map(myCorpus, content_transformer(tolower))
+  myCorpus = tm_map(myCorpus, removePunctuation)
+  myCorpus = tm_map(myCorpus, removeNumbers)
+  myCorpus = tm_map(myCorpus, removeWords,
+                    c(stopwords("SMART")))
+  
+  myDTM = TermDocumentMatrix(myCorpus,
+                             control = list(minWordLength = 1))
+  
+  m = as.matrix(myDTM)
+  
+  sort(rowSums(m), decreasing = TRUE)
+})
+
 
 shinyApp(ui = ui, server = server)
