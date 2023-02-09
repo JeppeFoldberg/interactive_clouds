a7221b1f83a34e8413e765ef7c8e6bac04a50c1f
Jeppe Foldberg <jeppe_05@msn.com>
2023-02-09 13:04:47 +0100

    Added stopwords, test-data and test script

---
+++ test.R
@@ -0,0 +1,69 @@
+pacman::p_load(
+  shiny,
+  tidyverse,
+  haven,
+  wordcloud2,
+  tm,
+  quanteda,
+  memoise
+)
+
+file <- read_sav("tekst_test_data.sav") %>% 
+  as_factor()
+
+text <- file$q9_22
+# attempt with Quanteda ---------------------------------------------------
+load_stopwords <- function() {
+  stop <- read.csv("stopord.txt", 
+                 sep = "\n",
+                 fileEncoding = "UTF-8",
+                 header = F) 
+
+  as.character(stop$V1)
+}
+
+
+count_words <- function(text_data,
+                        stopwords = c('ja'),
+                        mention_limit=10,
+                        patterns=NULL
+) {
+  tkns <- quanteda::tokens(text_data,
+                           remove_punct = TRUE,
+                           remove_symbols = TRUE,
+                           remove_numbers = TRUE,
+                           remove_url = TRUE,
+                           remove_separators = TRUE,
+                           split_hyphens = TRUE,
+                           include_docvars = TRUE,
+                           padding = F)
+  
+ 
+  dfm_question <- quanteda::dfm(x = tkns, tolower = TRUE)
+  
+  # problems? Search for dplyr and NSE (non standard evaluation)
+  # there is no abundant information about how to do it properly.
+  # https://shipt.tech/https-shipt-tech-advanced-programming-and-non-standard-evaluation-with-dplyr-e043f89deb3d
+  # This is the solution used (under the section 'a note on tidyverse')
+  # https://www.r-bloggers.com/2019/08/no-visible-binding-for-global-variable/
+  df <- dfm_question %>%
+    quanteda::convert(to = "data.frame") %>%
+    tidyr::pivot_longer(!.data$doc_id,
+                        names_to = "word",
+                        values_to = "count") %>%
+    dplyr::filter(.data$count > 0) %>%
+    dplyr::filter(!.data$word %in% stopwords)
+  
+  count <- df %>%
+    dplyr::group_by(.data$word) %>%
+    dplyr::summarise(mentions = sum(.data$count, na.rm = TRUE)) %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(.data$mentions >= mention_limit) %>% 
+    arrange(desc(.data$mentions))
+}
+
+temp <- count_words(file$q9_22, 
+                    stopwords = F,
+                    mention_limit = 1)
+
+wordcloud2(temp)
